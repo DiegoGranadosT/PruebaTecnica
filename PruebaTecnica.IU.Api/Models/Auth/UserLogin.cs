b1ddd3a0d79b535791ca78dcfef6272afa094aa8
DIEGO-GRANADOS\diego <diegocamilog@live.com>
2022-01-13 23:45:31 -0500

    Auth configuration

--- PruebaTecnica.IU.Api/Models/Auth/UserLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PruebaTecnica.IU.Api.Models.Auth
-{
-    public class UserLogin
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
